bc68567a5acb021db234c1f4abbb50f43ec9a0fa
varaprasad-1 <prasadkotyada001@gmail.com>
2026-03-11 21:02:36 +0530

    Update generator.py

--- generator.py
+++ generator.py
@@ -1,19 +1,46 @@
 import random
 from algorithm_templates import TEMPLATES
 from bug_injector import inject_bug
+from mutator import mutate_code
 
-generated=set()
+# copy templates so we can remove used ones
+available_templates = TEMPLATES.copy()
+
+# avoid duplicate programs
+generated = set()
+
 
 def generate_bug():
 
+    global available_templates
+
+    # stop when all templates used
+    if not available_templates:
+        return (
+            "All debugging challenges have been used.",
+            "No more challenges available.",
+            ""
+        )
+
     while True:
 
-        template=random.choice(TEMPLATES)
+        template = random.choice(available_templates)
 
-        buggy_code,fix=inject_bug(template)
+        # remove template so logic never repeats
+        available_templates.remove(template)
+
+        # apply mutations
+        template = mutate_code(template)
+
+        # inject bug
+        buggy_code, fix = inject_bug(template)
 
         if buggy_code and buggy_code not in generated:
 
             generated.add(buggy_code)
 
-            return buggy_code,fix
+            description = """
+Fix the bug in the following C program.
+"""
+
+            return description, buggy_code, fix
