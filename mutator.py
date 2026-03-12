38bbc8ac94ff972bc255262757a4fc141592dd91
varaprasad-1 <prasadkotyada001@gmail.com>
2026-03-12 23:06:02 +0530

    Update mutator.py

--- mutator.py
+++ mutator.py
@@ -2,24 +2,37 @@ import random
 import re
 
 VAR_NAMES = ["sum", "total", "result", "value"]
-COUNT_NAMES = ["count", "cnt", "counter"]
 ARRAY_NAMES = ["arr", "data", "nums", "values"]
 INDEX_NAMES = ["i", "j", "k"]
+
+CONSTANTS = [2,3,4,5,7,10]
 
 def replace_word(code, old, new):
     return re.sub(r"\b" + old + r"\b", new, code)
 
+
 def mutate_variables(code):
 
-    code = replace_word(code, "i", random.choice(INDEX_NAMES))
     code = replace_word(code, "arr", random.choice(ARRAY_NAMES))
     code = replace_word(code, "sum", random.choice(VAR_NAMES))
-    code = replace_word(code, "count", random.choice(COUNT_NAMES))
+    code = replace_word(code, "count", random.choice(VAR_NAMES))
+    code = replace_word(code, "i", random.choice(INDEX_NAMES))
 
     return code
+
+
+def mutate_constants(code):
+
+    for c in CONSTANTS:
+        if str(c) in code:
+            code = code.replace(str(c), str(random.randint(2,15)))
+
+    return code
+
 
 def mutate_code(code):
 
     code = mutate_variables(code)
+    code = mutate_constants(code)
 
     return code
